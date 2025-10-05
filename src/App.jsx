c5d5af058352f80fd802079993b0c46ad12fbbb9
Joyclare <cheronojoyclaire@gmail.com>
2025-10-05 13:56:06 +0300

    Updated App.jsx to show Login Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Login from "./components/Login";
 
 function App() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
-        Hello Tailwind 🚀
-      </button>
-    </div>
-  );
+  return <Login />;
 }
 
 export default App;
-
