87549152b93387f2ea9579f2299eafa840774eef
Joyclare <cheronojoyclaire@gmail.com>
2025-10-19 11:33:06 +0300

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import Login from "./components/Login";
+import React, { useState } from 'react'
+import AddCountdown from './components/AddCountdown'
+import Login from './components/Login'
 
 function App() {
-  return <Login />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-indigo-500 to-purple-600 text-white">
+      {!isLoggedIn ? (
+        <Login onLogin={() => setIsLoggedIn(true)} />
+      ) : (
+        <AddCountdown />
+      )}
+    </div>
+  )
 }
 
-export default App;
+export default App
